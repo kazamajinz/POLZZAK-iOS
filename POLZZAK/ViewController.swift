4b9e06f13983ad51e3582297d885f264c64313bd
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2023-05-11 00:54:38 +0900

    init project

---
+++ POLZZAK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POLZZAK
+//
+//  Created by Jinyoung Kim on 2023/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
