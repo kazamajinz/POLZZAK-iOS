75ce529d73706c900d52262045bf6fd3da195987
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2023-05-12 17:55:58 +0900

    [FEAT] add color assets, UIColor extension

---
+++ POLZZAK/Extensions/UIColor+extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+extension.swift
+//  POLZZAK
+//
+//  Created by Jinyoung Kim on 2023/05/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blue100 = UIColor(named: "blue100")!
+    static let blue200 = UIColor(named: "blue200")!
+    static let blue300 = UIColor(named: "blue300")!
+    static let blue400 = UIColor(named: "blue400")!
+    static let blue500 = UIColor(named: "blue500")!
+    static let blue600 = UIColor(named: "blue600")!
+}
