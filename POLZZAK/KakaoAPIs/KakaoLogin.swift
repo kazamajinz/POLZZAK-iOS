dc421acc54bdc5e09dbe5e3f30c7a8759bda0bf5
z3rosmith <52317025+z3rosmith@users.noreply.github.com>
2023-06-10 12:59:24 +0900

    [FEAT] add KakaoLoginAPI #3

---
+++ POLZZAK/KakaoAPIs/KakaoLogin.swift
@@ -0,0 +1,55 @@
+//
+//  KakaoLogin.swift
+//  POLZZAK
+//
+//  Created by ZERO on 2023/06/09.
+//
+
+import Foundation
+
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
+
+struct KakaoLoginAPI {
+    /// - Returns: accessToken
+    static func loginWithKakao() async throws -> String {
+        let oAuthToken: OAuthToken
+        if UserApi.isKakaoTalkLoginAvailable() {
+            oAuthToken = try await UserApi.shared.loginWithKakaoTalk()
+        } else {
+            oAuthToken = try await UserApi.shared.loginWithKakaoAccount()
+        }
+        return oAuthToken.accessToken
+    }
+    
+//    static func getKakaoAccessToken() async throws -> String {
+//        var accessToken: String?
+//        if AuthApi.hasToken() {
+//            do {
+//                let accessTokenInfo = try await UserApi.shared.accessTokenInfo()
+//
+//            } catch {
+//                if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true {
+//                    let oAuthToken = try await loginWithKakao()
+//                    accessToken = oAuthToken.accessToken
+//                }
+//            }
+//            UserApi.shared.accessTokenInfo { (_, error) in
+//                if let error = error {
+//                    if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true  {
+//                        //로그인 필요
+//                    }
+//                    else {
+//                        //기타 에러
+//                    }
+//                }
+//                else {
+//                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+//                }
+//            }
+//        } else {
+//            //로그인 필요
+//        }
+//    }
+}
--- POLZZAK/KakaoAPIs/UserApi+extension.swift
+++ POLZZAK/KakaoAPIs/UserApi+extension.swift
@@ -5,8 +5,20 @@
 //  Created by Jinyoung Kim on 2023/06/06.
 //
 
+import Foundation
+
 import KakaoSDKAuth
 import KakaoSDKUser
+
+enum KakaoUserAPIError: LocalizedError {
+    case noResponseData
+    
+    var errorDescription: String? {
+        switch self {
+        case .noResponseData: return "data와 error 모두 nil입니다."
+        }
+    }
+}
 
 extension UserApi {
     func loginWithKakaoTalk() async throws -> OAuthToken {
@@ -14,9 +26,38 @@ extension UserApi {
             loginWithKakaoTalk { oauthToken, error in
                 if let error {
                     continuation.resume(throwing: error)
+                } else if let oauthToken {
+                    continuation.resume(returning: oauthToken)
                 } else {
-                    // 강제 언래핑 하면 안될듯
-                    continuation.resume(returning: oauthToken!)
+                    continuation.resume(throwing: KakaoUserAPIError.noResponseData)
+                }
+            }
+        }
+    }
+    
+    func loginWithKakaoAccount() async throws -> OAuthToken {
+        return try await withCheckedThrowingContinuation { continuation in
+            loginWithKakaoAccount { oauthToken, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else if let oauthToken {
+                    continuation.resume(returning: oauthToken)
+                } else {
+                    continuation.resume(throwing: KakaoUserAPIError.noResponseData)
+                }
+            }
+        }
+    }
+    
+    func accessTokenInfo() async throws -> AccessTokenInfo {
+        return try await withCheckedThrowingContinuation { continuation in
+            accessTokenInfo { accessTokenInfo, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else if let accessTokenInfo {
+                    continuation.resume(returning: accessTokenInfo)
+                } else {
+                    continuation.resume(throwing: KakaoUserAPIError.noResponseData)
                 }
             }
         }
