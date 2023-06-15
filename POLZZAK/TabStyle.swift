787cbc2184f4f4c03a0abcf16c9bce51aba7b66b
kazamajinz <kazamajinz@naver.com>
2023-06-15 15:36:28 +0900

    [REFACTOR] Edit to TabViews #7

--- POLZZAK/TabStyle.swift
+++ POLZZAK/TabStyle.swift
@@ -15,13 +15,13 @@ enum TabStyle {
         case .linkManagement:
             return TabConfig(
                 textArray: ["연동 목록", "받은 요청", "보낸 요청"],
-                textColor: .gray600,
-                font: .subtitle3,
-                lineColor: .gray400,
-                lineHeight: 1.0,
+                textColor: .gray300,
+                font: .subtitle2,
+                lineColor: .gray300,
+                lineHeight: 2.0,
                 selectTextColor: .blue500,
                 selectLineColor: .blue500,
-                selectLineHeight: 4.0
+                selectLineHeight: 2.0
             )
         }
     }
