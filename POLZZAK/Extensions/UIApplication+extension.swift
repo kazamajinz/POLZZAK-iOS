9ba04ea8bbe55ce99245c421c0967ac46f020ded
kazamajinz <kazamajinz@naver.com>
2023-08-24 16:58:15 +0900

    [FIX] UIApplication+extension 수정 #16

--- POLZZAK/Extensions/UIApplication+extension.swift
+++ POLZZAK/Extensions/UIApplication+extension.swift
@@ -12,6 +12,7 @@ extension UIApplication {
         return UIApplication
             .shared
             .connectedScenes
+            .filter { $0.activationState == .foregroundActive }
             .compactMap { $0 as? UIWindowScene }
             .flatMap { $0.windows }
             .first { $0.isKeyWindow }
@@ -70,26 +71,5 @@ extension UIApplication {
             return getTopViewController(base: presented)
         }
         return base
-    }
-    
-    static func topViewController(controller: UIViewController? = UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .map { $0 as? UIWindowScene }
-            .compactMap { $0 }
-            .first?.windows
-            .filter { $0.isKeyWindow }.first?.rootViewController) -> UIViewController? {
-                
-        if let navigationController = controller as? UINavigationController {
-            return topViewController(controller: navigationController.visibleViewController)
-        }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
-        }
-        if let presented = controller?.presentedViewController {
-            return topViewController(controller: presented)
-        }
-        return controller
     }
 }
